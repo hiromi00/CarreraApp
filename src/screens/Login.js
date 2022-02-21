f07e860e5ece4098733213912674af0b9b7b1181
alejandro ortiz <cesar.ortizf00@gmail.com>
2022-02-20 19:34:01 -0600

    axios

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,7 +7,9 @@ import { WavyHeader } from 'app/components/WavyHeader';
 import { StyledButton } from 'app/components/StyledButton';
 import { TextInput } from 'app/components/TextInput';
 import { useFormik } from 'formik';
+//import * as axios from 'axios';
 import * as Yup from 'yup';
+const axios = require('axios');
 
 const useStyles = makeStyles({
   mainContainer: {
@@ -53,6 +55,16 @@ export const Login = ({ navigation }) => {
     validationSchema: loginSchema,
     onSubmit: (values) => {
       console.log(values);
+      const xd = axios.get(`https://marvelous-valley.000webhostapp.com/temp.php?login=${values.username}&pass=${values.password}`).then((response) => {console.log(response)});
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log(xhttp.responseText);
+        }
+      };
+      xhttp.open("GET", `https://marvelous-valley.000webhostapp.com/temp.php?login=${values.username}&pass=${values.password}`, true);
+      console.log(`https://marvelous-valley.000webhostapp.com/temp.php?login=${values.username}&pass=${values.password}`);
+      xhttp.send();
     },
   });
 
