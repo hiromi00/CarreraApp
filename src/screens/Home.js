bc5e901946887408fde6807e59997282c78ed8fa
César A. Ortíz F <cesar.ortizf00@gmail.com>
2022-03-30 23:54:00 -0600

    RunnerCard

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,49 +1,85 @@
+import { BRAND } from 'app/assets/images';
 import { SidebarView } from 'app/components/DrawerContent';
+import { RunnerCard } from 'app/components/RunnerCard';
+import { FormView } from 'app/layouts/FormView';
 import { getCorredoresRequest } from 'app/services/Connection';
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { makeStyles, Text } from 'react-native-elements';
+import { View,Image } from 'react-native';
+import {  makeStyles, Text } from 'react-native-elements';
 
-const useStyles = makeStyles({
+const runners = [
+  {
+    name: 'Samuel',
+    avatar: 'https://randomuser.me/api/portraits/men/36.jpg',
+  },
+  {
+    name: 'Luisa',
+    avatar:
+      'https://cdn.pixabay.com/photo/2014/09/17/20/03/profile-449912__340.jpg',
+  },
+  {
+    name: 'Camilo',
+    avatar:
+      'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg',
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  formView: {
+    backgroundColor: theme.colors.secondary,
+  },
   mainContainer: {
     height: '100%',
     maxHeight: '100%',
-    backgroundColor: 'white',
-  },
-  headerContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: theme.colors.secondary,
     flexDirection: 'column',
-    height: 400,
+    alignItems: 'center',
   },
   logo: {
-    height: 390,
+    height: 350,
     resizeMode: 'contain',
   },
-  text: {
-    fontSize: 30,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: 'green',
-    padding: 10
-  }
-});
+  userCount: {
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 30,
+  },
+}));
 
 export const Home = () => {
+  const [count, setCount] = useState(0);
   const styles = useStyles();
-  const [result, setResult] = useState(0);
+
+  const get = async () => {
+    await getCorredoresRequest().then(({ data }) => {
+      setCount(data);
+    });
+  };
+
   useEffect(() => {
-    const getCorredores = async () => {
-      getCorredoresRequest().then((response) => {
-        setResult(response.data);
-      });
-    };
-    getCorredores();
+    get();
   }, []);
 
   return (
     <SidebarView>
-      <Text style={styles.userCount}>Usuarios registrados: {`${result}`}</Text>
+      <FormView style={styles.formView}>
+        <View style={styles.mainContainer}>
+          <View style={styles.headerContainer}>
+            <Image source={BRAND} style={styles.logo} />
+          </View>
+          <Text style={styles.userCount}>
+            Usuarios registrados: {`${count}`}
+          </Text>
+          {runners.map((item, index) => (
+            <RunnerCard
+              key={index}
+              avatar={item.avatar}
+              name={item.name}
+              placement={index + 1}
+            />
+          ))}
+        </View>
+      </FormView>
     </SidebarView>
   );
 };
--- src/screens/MainStack.js
+++ src/screens/MainStack.js
@@ -1,31 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from 'app/screens/Login';
 import { SignUp } from './SignUp';
 import { Home } from './Home';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ActivityIndicator } from 'react-native';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export const MainStack = () => {
+  //const styles = useStyles();
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const get = async () => {
+    await AsyncStorage.getItem('DatosU').then((value) => {
+      if (value !== null) {
+        setIsSignedIn(true);
+      }
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    get();
+  }, []);
+
+  if (loading) return <ActivityIndicator size="large" color="blue" />;
+
   return (
     <NavigationContainer>
       <Navigator>
-        <Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={Login}
-        />
-        <Screen
-          options={{ headerShown: false }}
-          name="SignUp"
-          component={SignUp}
-        />
-        <Screen
-          options={{ headerShown: false }}
-          name="Home"
-          component={Home}
-        />
+        {isSignedIn ? (
+          <>
+            <Screen
+              options={{ headerShown: false }}
+              name="Home"
+              component={Home}
+            />
+          </>
+        ) : (
+          <>
+            <Screen
+              options={{ headerShown: false }}
+              name="Login"
+              component={Login}
+            />
+            <Screen
+              options={{ headerShown: false }}
+              name="SignUp"
+              component={SignUp}
+            />
+          </>
+        )}
       </Navigator>
     </NavigationContainer>
   );
