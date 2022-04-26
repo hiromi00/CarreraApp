bc8c33d6208586c053d0a672f2f2456ff002196f
César A. Ortíz F <cesar.ortizf00@gmail.com>
2022-04-26 00:25:07 -0500

    Se cambia la manera en la que se despelga el top 3

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,37 +1,23 @@
 import { BRAND } from 'app/assets/images';
 import { SidebarView } from 'app/components/DrawerContent';
 import { RunnerCard } from 'app/components/RunnerCard';
-import { FormView } from 'app/layouts/FormView';
 import { getCorredoresRequest } from 'app/services/Connection';
 import React, { useEffect, useState } from 'react';
 import { View,Image } from 'react-native';
 import {  makeStyles, Text } from 'react-native-elements';
-
-/* const runners = [
-  {
-    name: 'Samuel',
-    avatar: 'https://randomuser.me/api/portraits/men/36.jpg',
-  },
-  {
-    name: 'Luisa',
-    avatar:
-      'https://cdn.pixabay.com/photo/2014/09/17/20/03/profile-449912__340.jpg',
-  },
-  {
-    name: 'Camilo',
-    avatar:
-      'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg',
-  },
-]; */
+import { FlatList } from 'react-native';
 
 const useStyles = makeStyles((theme) => ({
-  formView: {
-    backgroundColor: theme.colors.secondary,
-  },
   mainContainer: {
     height: '100%',
+    width: '100%',
     maxHeight: '100%',
     backgroundColor: theme.colors.secondary,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  headerContainer: {
+    justifyContent: 'center',
     flexDirection: 'column',
     alignItems: 'center',
   },
@@ -50,13 +36,16 @@ export const Home = () => {
   const [count, setCount] = useState(0);
   const styles = useStyles();
   const [runners, setRunners] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
   const get = async () => {
+    setRefreshing(true);
     await getCorredoresRequest().then(({ data }) => {
       console.log('data-----------> ', data);
       setCount(data.total);
       setRunners(data.corredores);
     });
+    setRefreshing(false);
   };
 
   useEffect(() => {
@@ -65,25 +54,28 @@ export const Home = () => {
 
   return (
     <SidebarView>
-      <FormView style={styles.formView}>
-        <View style={styles.mainContainer}>
-          <View style={styles.headerContainer}>
-            <Image source={BRAND} style={styles.logo} />
-          </View>
+      <View style={styles.mainContainer}>
+      <FlatList 
+      onRefresh={get}
+      refreshing={refreshing}
+      ListHeaderComponent={
+        <View style={styles.headerContainer}>
+          <Image source={BRAND} style={styles.logo} />
           <Text style={styles.userCount}>
             Usuarios registrados: {`${count}`}
           </Text>
-          {runners?.map((item, index) => (
-            <RunnerCard
-              key={index}
-              avatar={item.foto}
-              name={item.nombre}
-              velocidad={item.velocidad}
-              placement={index + 1}
-            />
-          ))}
         </View>
-      </FormView>
+      }
+      data={runners}
+      renderItem={({item, index}) => (
+        <RunnerCard 
+        key={index}
+        avatar={item.foto}
+        name={item.nombre}
+        velocidad={item.velocidad}
+        placement={index + 1} />
+      )}/>
+      </View>
     </SidebarView>
   );
 };
